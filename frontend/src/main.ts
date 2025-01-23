9739448caa08e1e2f815c52230a291f02310ef09
happyhaha1 <474248257@qq.com>
2025-01-23 13:37:09 +0800

    初始化前端项目

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+const app = createApp(App)
+const router = createRouter({
+  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+})
+app.use(router)
+app.mount('#app')
